c83cc24697048c68f8f8b6797061917923e4f813
DavidCands1914 <dcs2@cesar.school>
2023-04-06 18:01:58 -0300

    Criando a Classe Tribo e Squad

--- src/cesarsquads/models.py
+++ src/cesarsquads/models.py
@@ -12,4 +12,22 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
     
-    # sfsfsfsf
+class Tribo(models.Model):
+    nameExibitionTribe = models.CharField(max_length=100)
+    descriptionTribe = models.CharField(max_length=300)
+    photoTribe = models.ImageField(upload_to='tribe_images', default='')
+    maxSquads = models.DecimalField(blank=True)
+    maxUserSquads = models.DecimalField(blank=True)
+    
+    def __str__(self):
+        return self.nameExibitionTribe
+    
+class Squad(models.Model):
+    nameExibitionSquad = models.CharField(max_length=100)
+    descriptionSquad = models.CharField(max_length=300)
+    photoSquad = models.ImageField(upload_to='squad_images', default='')
+    maxUsers = models.DecimalField(blank=True)
+    
+    def __str__(self):
+        return self.nameExibitionSquad
+    
