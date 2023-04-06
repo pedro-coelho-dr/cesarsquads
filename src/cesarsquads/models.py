7fb880714f14a49fdc3439d7d387ec46a166be16
DavidCands <94338706+DavidCands@users.noreply.github.com>
2023-04-06 16:14:50 -0300

    Criando o Profile

--- src/cesarsquads/models.py
+++ src/cesarsquads/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=12)
+    password2 = models.CharField(max_length=12)
+    bio = models.CharField(max_length=500)
+    photo = models.ImageField(upload_to='profile_images',default='')
+    
+    def __str__(self):
+        return self.name
+    
