7766b37922915c41073dd5edf01d6c6f3d393699
Pedro Coelho <pcdr@cesar.school>
2023-04-08 19:06:15 -0300

    ajustei a o logout para voltar para o login 	modified:   src/fdsproject/urls.py 	modified:   src/templates/index.html

--- src/fdsproject/urls.py
+++ src/fdsproject/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cesarsquads.urls')),
     path('login/', user_view.Login, name ='login'),
-    path('logout/', auth.LogoutView.as_view(template_name ='index.html'), name ='logout'),
+    path('logout/', auth.LogoutView.as_view(next_page='index'), name='logout'),
     path('register/', user_view.register, name ='register'),
 ]
 
