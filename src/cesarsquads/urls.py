eaf612f2ea26e76c0346cb2129ad2d55f084458f
fantonioluz <falf@cesar.school>
2023-04-08 09:03:21 -0300

    Att telas
    Algumas elencações não estavam funcionando

--- src/cesarsquads/urls.py
+++ src/cesarsquads/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
 
 
