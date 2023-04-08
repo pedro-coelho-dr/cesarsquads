371d63635f4d85066d2fe5dfe1020d999a5a2683
Pedro Coelho <pcdr@cesar.school>
2023-04-08 18:48:54 -0300

    detalhezin

--- src/cesarsquads/models.py
+++ src/cesarsquads/models.py
@@ -17,6 +17,9 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+        
+    def __str__(self):
+        return self.user.username
 
 class Tribe(models.Model):
     name = models.CharField(max_length=100)
@@ -26,7 +29,8 @@ class Tribe(models.Model):
     max_user_squad = models.IntegerField(blank=True)
     
     def __str__(self):
-        return self.name
+        return self.user.username
+    
     
 class Squad(models.Model):
     name = models.CharField(max_length=100)
@@ -35,5 +39,5 @@ class Squad(models.Model):
     max_users = models.IntegerField(blank=True)
     
     def __str__(self):
-        return self.name
+        return self.user.username
     
