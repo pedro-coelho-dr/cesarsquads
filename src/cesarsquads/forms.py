1580b53a4cb02024445c4bad97ca10b0f8905e73
Pedro Coelho <pcdr@cesar.school>
2023-04-09 12:01:06 -0300

    -Consegui ajeitar o forms pra não mostrar tudo automaticamente Temos mais controle do crispy forms
    -Refazendo do 'zero' manualmente os htmls usando o site do bootstrap5
    Mantive a antiga versão numa pasta por garantia
    	modified:   src/cesarsquads/forms.py
    	new file:   src/static/css/login.css
    	new file:   src/templates copy/index.html
    	new file:   src/templates copy/registration/login.html
    	new file:   src/templates copy/registration/register.html
    	modified:   src/templates/index.html
    	modified:   src/templates/registration/login.html
    	modified:   src/templates/registration/register.html

--- src/cesarsquads/forms.py
+++ src/cesarsquads/forms.py
@@ -3,8 +3,30 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'type': 'username',
+            'placeholder':('Username')
+        }
+    ))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={
+            'type':'email',
+            'placeholder':('Email')
+        }
+    ))
+    password1 = forms.CharField(max_length=16,widget=forms.PasswordInput(
+        attrs={
+            # 'class':'form-control',
+            'placeholder':'Password'
+        }
+    ))
+    password2 = forms.CharField(max_length=16,widget=forms.PasswordInput(
+        attrs={
+            # 'class':'form-control',
+            'placeholder':'Repeat Password'
+        }
+    ))
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
