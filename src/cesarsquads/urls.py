beeb2a8208c1df559603ad6b5703421b67d18cd4
fantonioluz <falf@cesar.school>
2023-04-17 16:12:26 -0300

    att

--- src/cesarsquads/urls.py
+++ src/cesarsquads/urls.py
@@ -3,7 +3,7 @@ from cesarsquads import views
 from django.contrib.auth import views as auth
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Login, name='index'),
     path('login/', views.Login, name ='login'),
     path('logout/', auth.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.register, name ='register'),
--- src/fdsproject/settings.py
+++ src/fdsproject/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "cesarsquads",
-    'bootstrap5',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
@@ -130,7 +129,7 @@ STATIC_ROOT = 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media'
 
 
 
