c3d073e5ba67b531cd2ebdb8ce2b404a39e05705
fantonioluz <falf@cesar.school>
2023-05-13 16:58:08 -0300

    att

--- src/cesarsquads/tests.py
+++ src/cesarsquads/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Create your tests here.
+
+class TestHome(LiveServerTestCase):
+    browser = webdriver.Chrome()
+   
+    def test_login(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        assert 'Login' in self.browser.title
+        
+        
