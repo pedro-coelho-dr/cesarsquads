28a059c30cd9c36aad3b6c8a934b0c3ee082e592
fantonioluz <falf@cesar.school>
2023-06-11 11:46:11 -0300

    Tudo equalizado

--- src/cesarsquads/urls.py
+++ src/cesarsquads/urls.py
@@ -19,5 +19,7 @@ urlpatterns = [
     path('tribe/<slug:tribe_id>/squad/<slug:squad_slug>/adicionar-usuario/', views.add_user_to_squad, name='add_user_to_squad'),
     path('tribe/<slug:tribe_id>/squad/<slug:squad_slug>/remover/', views.remove_user_from_squad, name='remove_user_from_squad'),
     path('search/', views.search_tribe, name='search_tribe'),
+    path('tribe/<slug:tribe_id>/squad/<slug:squad_slug>/sair/', views.sair_squad, name='sair_squad'),
+    path('tribe/<slug:tribe_slug>/sair/', views.sair_tribo, name='sair_tribo'),
 
 ]
